32973a21910291396aea84548865f119ee068ef7
Ketan Totlani <itsketant@gmail.com>
2021-08-10 20:19:05 +0530

    Update Definitions.js

--- src/Components/Definitions/Definitions.js
+++ src/Components/Definitions/Definitions.js
@@ -8,7 +8,7 @@ const Definitions = ({word, category,lightmode, meanings }) => {
             {
                 meanings[0] && word && category==='en' && (
                     <audio src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio } controls style={{backgroundColor:"#fff", borderRadius:10}}>
-                        Your Browser doesn't support audio element
+                        Your Browser doesn't support audio files
                     </audio>
                 )
             }
